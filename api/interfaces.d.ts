1754807d4924ebf99d57cd9c052029ac1ea29b4c
Tijl Van den Brugghen <me@tijlvdb.me>
2019-09-15 18:43:44 +0200

    :pencil: Documenting interfaces

--- api/interfaces.d.ts
+++ api/interfaces.d.ts
@@ -1,10 +1,15 @@
 import { DbUser } from './models/User'
 
+// Configuration as returned by dotenv. dotenv only returns strings.
 export interface Config {
+    // Dtabase URI for access to MySQL
     DB_URI: string
+
+    // Port the web server will listen on
     PORT: string
 }
 
+// Type of requests that exist for resources/models (like DbUser, DbSubtitle)
 type APIRequestType = 'list' | 'create' | 'read' | 'update' | 'delete'
 
 interface APIModelSettings<T> {
